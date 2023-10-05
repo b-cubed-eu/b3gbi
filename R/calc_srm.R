c492581e64d71cb744e1899eda1f6118d1feb1d5
Shawn Dove <shawn.dove@hotmail.com>
2023-10-05 22:30:56 +0200

    Large update, but not yet functional

--- R/calc_srm.R
+++ R/calc_srm.R
@@ -3,43 +3,164 @@
 # Input is ?, level should be "world", "continent", "country", or ?
 # region is the specific continent, country, etc., or "world" for global.
 # level and region therefore must match.
+# Arguments cs1 and cs2 are width and height (in km) of the grid.
 
-calc_srm <- function(data, ...) {
+calc_srm <- function(data, cs1, cs2, ...) {
 
   # Download and prepare Natural Earth map data for Europe
-  map_data <- ne_countries(scale = "medium", ...) %>%
+  map_data <- ne_countries(scale = "medium", country = "Germany") %>%
     st_as_sf() %>%
     st_transform(crs = "EPSG:3035")
 
-  # Filter the map data to only include Greece
-  greece <- map_data %>%
-    select(name = "Greece")
-
-  # Make a grid across Greece
-  grid_greece <- greece %>%
-    st_make_grid(cellsize = c(100000, 100000)) %>%
-    st_intersection(greece) %>%
+  # Make a grid across the map area
+  grid <- map_data %>%
+    st_make_grid(cellsize = c(cs1 * 1000, cs2 * 1000)) %>%
+    st_intersection(map_data) %>%
     st_cast("MULTIPOLYGON") %>%
     st_sf() %>%
     mutate(cellid = row_number())
 
+  # Set map limits
+  map_lims <- st_buffer(grid, dist = 1000) %>%
+    st_bbox()
+
+  # Scale coordinates of occurrences so the number of digits matches map
+  merged_data_scaled <-
+    merged_data %>%
+    mutate(xcoord = xcoord * 1000,
+           ycoord = ycoord * 1000)
+
+  # Convert the x and y columns to the correct format for plotting with sf
+  occ_sf <- st_as_sf(merged_data_scaled,
+                     coords = c("xcoord", "ycoord"),
+                     crs = "EPSG:3035")
+
+  # Calculate intersection between occurrences and grid cells
+  occ_grid_int <- st_intersection(occ_sf, grid, left = TRUE)
+
+  #  ----------------------------------------
+
   # Calculate species richness over the grid
-  richness_grid <- grid_greece %>%
-    st_join(occ_sf_greece) %>%
+  richness_grid <- grid %>%
+    st_join(occ_sf) %>%
     # mutate(overlap = ifelse(obs >= 1, 1, 0)) %>%
     group_by(cellid) %>%
     summarize(num_species = sum(n_distinct(obs)))
 
-  lims_greece <- st_buffer(greece, dist = 1000) %>% st_bbox()
+
+  #  ----------------------------------------
+
+  # Add cell numbers to occurrence data
+  merged_data_cells <-
+    merged_data_scaled %>%
+    dplyr::inner_join(occ_grid_int) %>%
+    dplyr::group_by(cellid)
+
+  # Create list of occurrence matrices by year, with species as rows
+  spec_rec_raw_cell <-
+    merged_data_cells %>%
+    dplyr::group_split() %>%
+    purrr::map(. %>%
+                 dplyr::group_by(eea_cell_code,
+                                 scientificName) %>%
+                 tidyr::pivot_wider(names_from = scientificName,
+                                    values_from = obs) %>%
+                 dplyr::ungroup() %>%
+                 dplyr::select(-eea_cell_code,
+                               -taxonKey,
+                               -kingdom,
+                               -rank,
+                               -geometry,
+                               -resolution,
+                               -xcoord,
+                               -ycoord,
+                               -year) %>%
+                 replace(is.na(.), 0) %>%
+                 dplyr::mutate_if(is.numeric,
+                                  as.integer) %>%
+                 dplyr::select(-cellid) %>%
+                 tibble::rownames_to_column() %>%
+                 tidyr::gather(variable,
+                               value,
+                               -rowname) %>%
+                 tidyr::spread(rowname, value) %>%
+                 'row.names<-'(., NULL) %>%
+                 tibble::column_to_rownames(var = "variable") %>%
+                 as.matrix() %>%
+                 replace(. > 1, as.integer(1))
+    )
+
+
+  # name list elements
+  names(spec_rec_raw_cell) <- unique(occ_grid_int$cellid)
+
+  # set sample size for inext function call
+  inext_sampsize <- 150
+
+  # Calculate diversity estimates
+  coverage_rare_cell <- spec_rec_raw_cell %>%
+    iNEXT(endpoint=inext_sampsize, knots = 20, nboot = 50, datatype="incidence_raw")
+
+  # Extract estimated relative species richness
+  est_richness <-
+    coverage_rare_temp$iNextEst$coverage_based %>%
+    dplyr::filter(t == inext_sampsize) %>%
+    dplyr::select(Assemblage, qD) %>%
+    dplyr::rename(year = Assemblage,
+                  est_relative_richness = qD)
+
+  # Calculate estimated relative richness as an index
+  est_richness <-
+    est_richness %>%
+    mutate(index = est_relative_richness/lag(est_relative_richness)) %>%
+    replace(is.na(.), 1) %>%
+    mutate(index = cumprod(index))
+
+
+
+
+  # Calculate rarefaction curves for each year
+  future::plan(multisession)
+  spec_rare_cell <- spec_rec_raw_cell %>%
+    furrr::future_map(vegan::specaccum, method = "rarefaction")
+
+  # Get the sampling effort level at which to interpolate
+  sampling_effort <-
+    spec_rare %>%
+    purrr::map(~max(.$individuals)) %>%
+    unlist() %>%
+    min()
+
+  # Interpolate richness at different sampling effort levels
+  interpolated_richness <-
+    spec_rare %>%
+    purrr::map(~stats::approx(.$sites, .$richness, xout = sampling_effort)$y)
+
+  # Calculate mean rarefied richness for each year
+  mean_rarefied_richness <-
+    interpolated_richness %>%
+    purrr::map(mean, na.rm=TRUE)
+
+  # Calculate adjusted species richness by year
+  rarefied_df <-
+    richness_by_year %>%
+    tibble::add_column(mean_rarefied_richness = unlist(mean_rarefied_richness),
+                       .after = "obs_richness") %>%
+    dplyr::mutate(adj_richness = (obs_richness - mean_rarefied_richness),
+                  .after = "mean_rarefied_richness")
+
+
+
+  # -------------------------------------------
 
   # Plot richness
   richness_plot <-  ggplot(richness_grid) +
-    geom_sf(data = greece, fill = "grey") +
+    #geom_sf(data = map_data, fill = "grey") +
     geom_sf(aes(fill = log(num_species + 1)), color = NA) +
     scale_fill_scico(palette = "davos", direction = -1, end = 0.9) +
     coord_sf(
-      xlim = c(lims_greece["xmin"], lims_greece["xmax"]),
-      ylim = c(lims_greece["ymin"], lims_greece["ymax"])
+      xlim = c(map_lims["xmin"], map_lims["xmax"]),
+      ylim = c(map_lims["ymin"], map_lims["ymax"])
     ) +
     scale_x_continuous() +
     theme(
@@ -47,130 +168,11 @@ calc_srm <- function(data, ...) {
       panel.background = element_rect(fill = "#2F4051"),
       panel.grid = element_blank(),
       line = element_blank(),
-      rect = element_blank()
-    ) + labs(fill = "richness")
+      rect = element_blank(),
+      legend.text = element_blank()
+    ) +
+    labs(fill = "log of \nrichness")
   richness_plot
-
-
-
-
-  # Calculate intersection between occurrences and grid cells
-  occ_grid_int_greece <- st_intersection(occ_sf_greece, grid_greece, left = TRUE)
-
-  # Extract coordinates of intersection points and create cell_id column
-  occ_grid_coords_greece <- occ_grid_int_greece %>%
-    st_coordinates() %>%
-    as_tibble() %>%
-    mutate(year = occ_grid_int_greece$year,
-           obs = occ_grid_int_greece$obs,
-           cell_id = paste0(X, "_", Y))
-
-  # Aggregate by cell ID, year and count unique taxa
-  richness_year_greece2 <- occ_grid_coords_greece %>%
-    group_by(cell_id, year) %>%
-    summarize(taxon_count = n_distinct(obs))
-
-  # Join richness data with grid data
-  grid_richness_greece <- grid_greece %>%
-    st_join(st_as_sf(occ_grid_coords_greece, coords = c("X", "Y"), crs = "EPSG: 3035")) %>%
-    group_by(cell_id)
-
-  # Create a base map of Greece
-  base_map <- ggplot() +
-    geom_sf(data = greece, fill = "lightgray", color = "gray")   # Plot Greece
-
-  # Create a plot of the grid with richness values
-  richness_map <- base_map +
-    geom_sf(data = grid_richness_greece, aes(fill = obs)) +  # Plot the grid with richness values
-    scale_fill_gradient(low = "white", high = "red") +  # Customize the color scale
-    labs(title = "Richness Values in Greece by Year") +
-    theme_minimal()
-
-  richness_map
-
-
-
-
-
-
-  # Scale the coordinates of occurrences so the number of digits matches
-  merged_data_scaled <-
-    merged_data %>%
-    mutate(xcoord = xcoord * 1000, ycoord = ycoord * 1000)
-
-  # Convert the x and y columns to the correct format for plotting with sf
-  occ_sf <- st_as_sf(merged_data_scaled, coords = c("xcoord", "ycoord"), crs = "EPSG:3035")
-
-  # Define EEA coordinate system
-  crs_eea <- st_crs("EPSG:3035")
-
-  # Define grid spacing and extent
-  grid_spacing <- 1000 # 1 km
-  grid_extent <- st_bbox(richness_year) %>%
-    st_as_sfc() %>%
-    st_set_crs(crs_eea) %>%
-    st_buffer(grid_spacing * 2) %>%
-    st_bbox()
-
-  # Create grid
-  grid <- st_make_grid(st_as_sfc(grid_extent), cellsize = grid_spacing) %>%
-    st_sf() %>%
-    mutate(cell_id = paste0(X, "_", Y))
-
-  # Transform grid to EEA coordinate system
-  grid_eea <- st_transform(grid, crs = crs_eea)
-
-  # Calculate intersection between occurrences and grid cells
-  occ_grid_int <- st_intersection(occ_sf, grid, left = TRUE)
-
-  # Extract coordinates of intersection points and create cell_id column
-  occ_grid_coords <- occ_grid_int %>%
-    st_coordinates() %>%
-    as_tibble() %>%
-    mutate(year = occ_grid_int$year,
-           obs = occ_grid_int$obs,
-           cell_id = paste0(X, "_", Y))
-
-  # # Aggregate by cell ID, year and count unique taxa
-  # richness_year <- occ_grid_coords %>%
-  #  group_by(cell_id, year) %>%
-  #  summarize(taxon_count = n_distinct(obs))
-  #
-  # # Join richness data with grid data
-  # grid_richness <- st_join(richness_year, grid_eea, left = TRUE)
-
-  # Aggregate by cell ID, year and count unique taxa
-  richness_year <- occ_sf %>%
-    group_by(eea_cell_code, year) %>%
-    summarise(geometry = first(geometry), taxon_count = n_distinct(obs))
-
-  saveRDS(richness_year, file="richness_year.RDS")
-
-
-  year_to_plot <- 2020
-  europe <- map_data_tr
-
-  ggplot() +
-    geom_sf(data = europe, fill = "white", color = "black") +
-    geom_tile(data = subset(richness_year, year == year_to_plot),
-              aes(x = st_coordinates(geometry)[, 1], y = st_coordinates(geometry)[, 2], fill = taxon_count),
-              alpha = 0.7) +
-    scale_fill_gradient(low = "gray90", high = "blue", na.value = "gray90") +
-    labs(title = paste("Species Richness Heatmap for Year", year_to_plot)) +
-    theme_minimal()
-
-
-
-  # Plot map with background layer of Greece, grid cells based on richness values, and occurrence data in 2022
-  ggplot() +
-    geom_sf(data = greece, fill = "lightgray") +
-    geom_sf(data = filter(richness_year, year == 2021), aes(fill = taxon_count)) +
-    scale_fill_gradient(low = "white", high = "blue") +
-    #geom_sf(data = filter(occ_sf, year == 2021), color = "red", size = 0.5) +
-    theme_void() +
-    labs(title = "Occurrence data and taxon richness in Greece in 2021")
-
-
 
 
 
