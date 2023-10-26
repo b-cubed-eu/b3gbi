b698c6cb267607ac19992c4705bdafaaf8988725
Shawn Dove <shawn.dove@hotmail.com>
2023-10-27 00:04:27 +0200

    Added another evenness formula

--- R/calc_evenness.R
+++ R/calc_evenness.R
@@ -14,3 +14,19 @@ calc_evenness <- function(x) {
   }
   return(even)
 }
+
+calc_e9_evenness <- function(x) {
+  n <- colSums(x)
+  N <- sum(n)
+  S <- sum(n > 0)
+  p <- n / N
+  p_squared <- p^2
+  summed <- sum(p_squared) * S
+  adjusted <- (summed - 1) / (S - 1)
+  root <- adjusted^(1/2)
+  even <- 1 - root
+  if (is.nan(even)) {
+    even <- NA
+  }
+  return(even)
+}
