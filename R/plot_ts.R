eeb2d5bf545a2a70551cd5cf8a8dbefe1c75dfc3
Shawn Dove <shawn.dove@hotmail.com>
2023-11-09 22:56:26 +0100

    Can plot multiple data types as separate trends

--- R/plot_ts.R
+++ R/plot_ts.R
@@ -5,37 +5,81 @@ plot_ts <- function(coverage_df) {
   final_year <- tail(coverage_df$year, n=1)
 
   # Determine legend label
-  diversity_type <- c("total_records",
+  diversity_types <- c("total_records",
                       "rarefied",
                       "0",
                       "1",
                       "2",
                       "obs_richness",
-                      "evenness")
+                      "evenness",
+                      "total_obs",
+                      "cum_richness")
 
-  names(diversity_type) <- c("Adjusted Species Richness",
+  names(diversity_types) <- c("Adjusted Species Richness",
                              "Rarefied Species Richness",
                              "Estimated Species Richness",
                              "Shannon Diversity",
                              "Simpson Diversity",
                              "Observed Species Richness",
-                             "Evenness")
+                             "Evenness",
+                             "Total Observations",
+                             "Cumulative Species Richness")
+
+  div_type <- dplyr::first(coverage_df$diversity_type, na_rm = TRUE)
 
   y_label <- names(diversity_type)[diversity_type %in%
-                                     coverage_df$diversity_type[1]]
+                                     div_type]
 
-  # Suppress y axis values except for certain diversity types
-  suppress_y <- if (coverage_df$diversity_type[1]=="obs_richness" |
-                    coverage_df$diversity_type[1]=="evenness" |
-                    coverage_df$diversity_type[1]=="total_records")
-    {
-    "n"
+  # # Suppress y axis values except for certain diversity types
+  # suppress_y <- if (div_type=="obs_richness" |
+  #                   div_type=="evenness" |
+  #                   div_type=="total_records")
+  #   {
+  #   "n"
+  # } else {
+  #   "y"
+  # }
+  suppress_y <- "n"
+
+  if ("type" %in% colnames(coverage_df)) {
+
+    # Plot trend
+    trend_plot <-
+      ggplot(coverage_df, aes(x = year,
+                              y = diversity_val)) +
+      geom_line(aes(color = type),
+                size = 0.8) +
+      # geom_smooth(aes(color = type),
+      #             size = 1,
+      #             linetype = "dashed",
+      #             method = "loess",
+      #             formula = "y ~ x") +
+      scale_x_continuous(breaks = seq(first_year,
+                                      final_year,
+                                      by = 5)) +
+      labs(x = "Year", y = y_label,
+           title = paste(y_label,
+                         " Trend",
+                         " (",
+                         first_year,
+                         "-",
+                         final_year,
+                         ")",
+                         sep="")) +
+      theme_minimal() +
+      theme(plot.title = element_text(hjust = 0.5),
+            text = element_text(size = 14),
+            axis.text.y = if (suppress_y=="y") {
+              element_blank()
+            } else {
+              element_text()
+            }
+      )
+
   } else {
-    "y"
-  }
 
     # Plot adjusted cube richness trend
-  adj_richness_ts_plot <-
+  trend_plot <-
       ggplot(coverage_df, aes(x = year,
                               y = diversity_val)) +
       geom_line(color = "lightblue",
@@ -45,9 +89,7 @@ plot_ts <- function(coverage_df) {
                   linetype = "dashed",
                   method = "loess",
                   formula = "y ~ x") +
-      scale_x_continuous(breaks = seq(first_year,
-                                      final_year,
-                                      by = 5)) +
+      scale_x_continuous(breaks = pretty_breaks(n=10)) +
       labs(x = "Year", y = y_label,
            title = paste(y_label,
                          " Trend",
@@ -67,6 +109,8 @@ plot_ts <- function(coverage_df) {
           }
     )
 
-  adj_richness_ts_plot
+  }
+
+  trend_plot
 
 }
