427e6bea3c7a9686edb977e4186d94ae4ea69cd8
Shawn Dove <shawn.dove@hotmail.com>
2023-11-29 15:18:48 +0100

    List the species in a tibble or data frame

---
+++ R/list_species.R
@@ -0,0 +1,10 @@
+list_species <- function(data) {
+
+  species_df <-
+    data %>%
+    dplyr::select(taxonKey, scientificName) %>%
+    dplyr::distinct(taxonKey, scientificName)
+
+  return(species_df)
+
+}
