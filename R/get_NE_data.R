1f12247518de3ecd9d9ee5d7ced663b6505301ce
Shawn Dove <shawn.dove@hotmail.com>
2024-03-01 02:26:17 +0100

    Added returnclass argument because sp is being deprecated

--- R/get_NE_data.R
+++ R/get_NE_data.R
@@ -6,20 +6,23 @@ get_NE_data <- function(level, region) {
   if (level == "country") {
 
     map_data <- rnaturalearth::ne_countries(scale = "medium",
-                                            country = region) %>%
+                                            country = region,
+                                            returnclass = "sf") %>%
       sf::st_as_sf() %>%
       sf::st_transform(crs = "EPSG:3035")
 
   } else if (level == "continent") {
 
     map_data <- rnaturalearth::ne_countries(scale = "medium",
-                                            continent = region) %>%
+                                            continent = region,
+                                            returnclass = "sf") %>%
       sf::st_as_sf() %>%
       sf::st_transform(crs = "EPSG:3035")
 
   } else if (level == "world") {
 
-    map_data <- rnaturalearth::ne_countries(scale = "medium") %>%
+    map_data <- rnaturalearth::ne_countries(scale = "medium",
+                                            returnclass = "sf") %>%
       sf::st_as_sf() %>%
       sf::st_transform(crs = "EPSG:3035")
 
