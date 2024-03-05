abb4f99ad20fbad56cbc4d7f6641d25759a05994
Shawn Dove <shawn.dove@hotmail.com>
2024-03-05 14:42:12 +0100

    Aesthetic changes to logo

--- make_hexSticker.R
+++ make_hexSticker.R
@@ -1,19 +1,19 @@
 hexSticker::sticker(
-  filename = "inst/figures/logo.png",
+  filename = "man/figures/logo.png",
   # subplot aesthetics
-  subplot = "inst/figures/b3.png",
-  s_width = 1.0, s_height = 1,
-  s_x=1, s_y=1,
+  subplot = "inst/figures/b3_b.png",
+  s_width = 0.41, s_height = 0.41,
+  s_x=1, s_y=0.67,
   # package name aesthetics
   package = "b3gbi",
-  p_size = 24,
-  p_color = "#005957",
+  p_size = 30,
+  p_color = "#48A529",
   # hexagon aesthetics
   h_size = 1,
-  h_fill = "white",
-  h_color = "#005957",
+  h_fill = "black",
+  h_color = "#48A529",
   # url aesthetics
   url = "https://github.com/ShawnDove/b3gbi",
   u_size = 4.2,
-  u_color = "#48A529"
+  u_color = "black"
 ) |> plot() # preview with plot()
