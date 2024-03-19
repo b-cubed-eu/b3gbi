8e392205e0ba7912f1489f5d045d211cee790a3c
Shawn Dove <shawn.dove@hotmail.com>
2024-03-19 14:26:55 +0100

    wrappers for compute_indicator_workflow

---
+++ R/indicator_wrappers.R
@@ -0,0 +1,222 @@
+#' @export
+obs_richness_map <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "obs_richness",
+                             dim_type = "map",
+                             ...)
+}
+
+#' @export
+obs_richness_ts <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "obs_richness",
+                             dim_type = "ts",
+                             ...)
+}
+
+#' @export
+total_occ_map <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "total_occ",
+                             dim_type = "map",
+                             ...)
+}
+
+#' @export
+total_occ_ts <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "total_occ",
+                             dim_type = "ts",
+                             ...)
+}
+
+#' @export
+pielou_evenness_map <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "pielou_evenness",
+                             dim_type = "map",
+                             ...)
+}
+
+#' @export
+pielou_evenness_ts <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "pielou_evenness",
+                             dim_type = "ts",
+                             ...)
+}
+
+#' @export
+williams_evenness_map <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "williams_evenness",
+                             dim_type = "map",
+                             ...)
+}
+
+#' @export
+williams_evenness_ts <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "williams_evenness",
+                             dim_type = "ts",
+                             ...)
+}
+
+#' @export
+area_rarity_map <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "area_rarity",
+                             dim_type = "map",
+                             ...)
+}
+
+#' @export
+area_rarity_ts <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "area_rarity",
+                             dim_type = "ts",
+                             ...)
+}
+
+#' @export
+ab_rarity_map <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "ab_rarity",
+                             dim_type = "map",
+                             ...)
+}
+
+#' @export
+ab_rarity_ts <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "ab_rarity",
+                             dim_type = "ts",
+                             ...)
+}
+
+#' @export
+hill0_map <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "hill0",
+                             dim_type = "map",
+                             ...)
+}
+
+#' @export
+hil0_ts <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "hill0",
+                             dim_type = "ts",
+                             ...)
+}
+
+#' @export
+hill1_map <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "hill1",
+                             dim_type = "map",
+                             ...)
+}
+
+#' @export
+hill1_ts <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "hill1",
+                             dim_type = "ts",
+                             ...)
+}
+
+#' @export
+hill2_map <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "hill2",
+                             dim_type = "map",
+                             ...)
+}
+
+#' @export
+hill2_ts <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "hill2",
+                             dim_type = "ts",
+                             ...)
+}
+
+#' @export
+cum_richness_ts <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "cum_richness",
+                             dim_type = "ts",
+                             ...)
+}
+#' @export
+newness_map <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "newness",
+                             dim_type = "map",
+                             ...)
+}
+
+#' @export
+occ_density_map <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "occ_density",
+                             dim_type = "map",
+                             ...)
+}
+
+#' @export
+occ_density_ts <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "occ_density",
+                             dim_type = "ts",
+                             ...)
+}
+
+#' @export
+spec_occ_map <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "spec_occ",
+                             dim_type = "map",
+                             ...)
+}
+
+#' @export
+spec_occ_ts <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "spec_occ",
+                             dim_type = "ts",
+                             ...)
+}
+
+#' @export
+spec_range_map <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "spec_range",
+                             dim_type = "map",
+                             ...)
+}
+
+#' @export
+spec_range_ts <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "spec_range",
+                             dim_type = "map",
+                             ...)
+}
+
+#' @export
+tax_distinct_map <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "tax_distinct",
+                             dim_type = "map",
+                             ...)
+}
+
+#' @export
+tax_distinct_ts <- function(data, ...) {
+  compute_indicator_workflow(data,
+                             type = "tax_distinct",
+                             dim_type = "ts",
+                             ...)
+}
