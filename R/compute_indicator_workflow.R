f1f31a0bfe31bf45c4b7acae006b67dab2983a1a
Shawn Dove <shawn.dove@hotmail.com>
2024-03-19 14:20:59 +0100

    merge map and ts, update documentation

--- R/compute_indicator_workflow.R
+++ R/compute_indicator_workflow.R
@@ -1,4 +1,4 @@
-#' @title Calculate Gridded Biodiversity Indicators
+#' @title Calculate Biodiversity Indicators Over Space or Time
 #'
 #' @description This function provides a flexible framework for calculating various biodiversity
 #' indicators on a spatial grid. It prepares the data, creates a grid, calculates indicators,
@@ -15,37 +15,38 @@
 #'   * 'ab_rarity', 'area_rarity':  Abundance-based and area-based rarity scores.
 #'   * 'spec_occ': Species occurrences.
 #'   * 'tax_distinct': Taxonomic distinctness.
-#' @param cs1 Length of the grid cell's sides, in kilometers, if square. (Default: 100)
-#' @param cs2 Width of the grid cell's sides, in kilometers. (Default: 100)
+#' @param cell_size Length of grid cell sides, in km. (Default: 10 for country, 100 for continent or world)
 #' @param level Spatial level: 'continent', 'country', or 'world'. (Default: 'continent')
 #' @param region The region of interest (e.g., "Europe"). (Default: "Europe")
 #' @param ... Additional arguments passed to specific indicator calculation functions.
 #'
 #' @return An S3 object of the appropriate class containing the calculated indicator values and metadata:
-#'   * 'indicator_map' for real-world observational data.
-#'   * 'virtual_indicator_map' for virtual species data.
+#'   * 'indicator_map' for real-world observational data calculated over a grid (map)
+#'   * 'indicator_ts' for real-world observational data calculated over time (time series)
+#'   * 'virtual_indicator_map' for virtual species data calculated over a grid (map).
 #'
 #' @examples
 #' # Assuming 'my_data_cube' is a 'processed_cube' or 'virtual_cube' object
 #' diversity_map <- calculate_indicator(my_data_cube, type = "obs_richness", level = "continent", region = "Africa")
 #'
 #' @export
-indicator.map <- function(x,
-                          type,
-                          cs1 = 100,
-                          cs2 = 100,
-                          level = c("continent", "country", "world"),
-                          region = "Europe",
-                          ...) {
+compute_indicator_workflow <- function(x,
+                                       type,
+                                       dim_type = c("map", "cant ts"),
+                                       cell_size = NULL,
+                                       level = c("continent", "country", "world"),
+                                       region = "Europe",
+                                       ...) {
 
 stopifnot_error("Object class not recognized.",
                 inherits(x, "processed_cube") |
                   inherits(x, "processed_cube_dsinfo") |
                   inherits(x, "virtual_cube"))
 
-level <- match.arg(level)
 type <- match.arg(type,
-                  c(available_indicators$indicator_class))
+                  names(available_indicators))
+dim_type <- match.arg(dim_type)
+level <- match.arg(level)
 
 data <- x$data
 
@@ -55,6 +56,16 @@ first_year <- x$first_year
 last_year <- x$last_year
 num_years <- length(unique(data$year))
 
+if (dim_type == "ts") {
+
+  year_names <- unique(data$year)
+  map_lims <- unlist(list("xmin" = min(data$xcoord),
+                          "xmax" = max(data$xcoord),
+                          "ymin" = min(data$ycoord),
+                          "ymax" = max(data$ycoord)))
+
+}
+
 if (!inherits(x, "virtual_cube")) {
 
   kingdoms <- x$kingdoms
@@ -63,128 +74,52 @@ if (!inherits(x, "virtual_cube")) {
 
 }
 
-# Download Natural Earth data
-map_data <- get_NE_data(level, region)
+if (dim_type == "map" | (!is.null(level) & !is.null(region))) {
 
-# Create grid from Natural Earth data
-grid <- create_grid(map_data, cs1, cs2)
+  # Download Natural Earth data
+  map_data <- get_NE_data(level, region)
 
-# Format spatial data and merge with grid
-data <- prepare_spatial_data(data, grid)
+  # Create grid from Natural Earth data
+  grid <- create_grid(map_data, level, cell_size)
 
-# Assign class to send data to correct calculator function
-attr(data, "class") <- type
-
-# Calculate indicator
-indicator <- calc_map(data, ...)
-
-# Add indicator values to grid
-diversity_grid <-
-  grid %>%
-  dplyr::left_join(indicator, by = "cellid")
-
-# Create indicator object
-if (!inherits(x, "virtual_cube")) {
-
-  diversity_obj <- new_indicator_map(diversity_grid,
-                                 div_type = type,
-                                 cs1 = cs1,
-                                 cs2 = cs2,
-                                 map_level = level,
-                                 map_region = region,
-                                 kingdoms = kingdoms,
-                                 num_species = num_species,
-                                 first_year = first_year,
-                                 last_year = last_year,
-                                 num_years = num_years,
-                                 species_names = species_names,
-                                 years_with_obs = years_with_obs)
+  # Format spatial data and merge with grid
+  data <- prepare_spatial_data(data, grid)
 
 } else {
 
-  diversity_obj <- new_virtual_indicator_map(diversity_grid,
-                                   div_type = type,
-                                   cs1 = cs1,
-                                   cs2 = cs2,
-                                   map_level = level,
-                                   map_region = region,
-                                   num_species = num_species,
-                                   first_year = first_year,
-                                   last_year = last_year,
-                                   num_years = num_years)
+  level <- "unknown"
+  region <- "unknown"
 
 }
 
-return(diversity_obj)
+# Assign class to send data to correct calculator function
+class(data) <- append(type, class(data))
 
-}
+if (dim_type == "map") {
 
+  # Calculate indicator
+  indicator <- calc_map(data, ...)
 
+  # Add indicator values to grid
+  diversity_grid <-
+    grid %>%
+    dplyr::left_join(indicator, by = "cellid")
 
-
-indicator.ts <- function(x,
-                          type,
-                          cs1 = 100,
-                          cs2 = 100,
-                          level = c("continent", "country", "world"),
-                          region = "Europe",
-                          ...) {
-
-  stopifnot_error("Object class not recognized.",
-                  inherits(x, "processed_cube") |
-                    inherits(x, "processed_cube_dsinfo") |
-                    inherits(x, "virtual_cube"))
-
-  level <- match.arg(level)
-  type <- match.arg(type,
-                    c(available_indicators$indicator_class))
-
-  data <- x$data
-
-  # Collect information to add to final object
-  num_species <- x$num_species
-  first_year <- x$first_year
-  last_year <- x$last_year
-  num_years <- length(unique(data$year))
-
-  if (!inherits(x, "virtual_cube")) {
-
-    kingdoms <- x$kingdoms
-    species_names <- unique(data$scientificName)
-    years_with_obs <- unique(data$year)
-
-  }
-
-  if (!is.null(level) & !is.null(region)) {
-
-    # Download Natural Earth data
-    map_data <- get_NE_data(level, region)
-
-    # Create grid from Natural Earth data
-    grid <- create_grid(map_data, cs1, cs2)
-
-    # Format spatial data and merge with grid
-    data <- prepare_spatial_data(data, grid)
-
-  } else {
-    level <- "unknown"
-    region <- "unknown"
-  }
-
-  # Assign class to send data to correct calculator function
-  attr(data, "class") <- type
+} else {
 
   # Calculate indicator
   indicator <- calc_ts(data, ...)
 
+}
 
-  # Create indicator object
-  if (!inherits(x, "virtual_cube")) {
+# Create indicator object
+if (!inherits(x, "virtual_cube")) {
 
-    diversity_obj <- new_indicator_ts(indicator,
+  if (dim_type == "map") {
+
+    diversity_obj <- new_indicator_map(diversity_grid,
                                        div_type = type,
-                                       cs1 = cs1,
-                                       cs2 = cs2,
+                                       cell_size = cell_size,
                                        map_level = level,
                                        map_region = region,
                                        kingdoms = kingdoms,
@@ -197,19 +132,32 @@ indicator.ts <- function(x,
 
   } else {
 
-    diversity_obj <- new_virtual_indicator_ts(indicator,
-                                               div_type = type,
-                                               cs1 = cs1,
-                                               cs2 = cs2,
-                                               map_level = level,
-                                               map_region = region,
-                                               num_species = num_species,
-                                               first_year = first_year,
-                                               last_year = last_year,
-                                               num_years = num_years)
+    diversity_obj <- new_indicator_ts(as_tibble(indicator),
+                                      div_type = type,
+                                      map_level = level,
+                                      map_region = region,
+                                      kingdoms = kingdoms,
+                                      num_species = num_species,
+                                      num_years = num_years,
+                                      species_names = species_names,
+                                      coord_range = map_lims)
 
   }
 
-  return(diversity_obj)
+} else {
+
+  diversity_obj <- new_virtual_indicator_map(diversity_grid,
+                                   div_type = type,
+                                   cell_size = cell_size,
+                                   map_level = level,
+                                   map_region = region,
+                                   num_species = num_species,
+                                   first_year = first_year,
+                                   last_year = last_year,
+                                   num_years = num_years)
+
+}
+
+return(diversity_obj)
 
 }
