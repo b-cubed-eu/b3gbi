bc25aada1e51a34a200c57a1d4dcf48e3e6e24d2
Shawn Dove <shawn.dove@hotmail.com>
2024-03-21 17:26:55 +0100

    now gets metadata from .rda file

--- R/get_divname.R
+++ R/get_divname.R
@@ -15,36 +15,38 @@
 #' @noRd
 get_divname <- function(x) {
 
-  div_df <- matrix(c("obs_richness", "Observed Species Richness",
-                     "cum_richness", "Cumulative Species Richness",
-                     "total_occ", "Total Occurrences",
-                     "occ_by_type", "Total Occurrences Segrerated by Type",
-                     "occ_by_dataset", "Total Occurrences Segregated by Dataset",
-                     "rarefied", "Estimated Species Richness",
-                     "hill0", "Estimated Species Richness",
-                     "hill1", "Estimated Shannon-Hill Diversity",
-                     "hill2", "Estimated Simpson-Hill Diversity",
-                     "e9_evenness", "E9 Evenness",
-                     "pielou_evenness", "Pielou's Evenness",
-                     "species_rarity", "Species Rarity",
-                     "newness", "Mean Year of Occurrences",
-                     "density", "Density of Occurrences",
-                     "ab_rarity", "Abundance-Based Rarity",
-                     "area_rarity", "Area-Based Rarity",
-                     "spec_occ", "Species Occurrences",
-                     "spec_range", "Species Ranges",
-                     "tax_distinct", "Taxonomic Distinctness"
-                  ),
-                  ncol = 2,
-                  byrow = TRUE) %>%
-    as.data.frame
+  iname <- as.character(available_indicators[[x]]$indicator_name)
 
-  names(div_df) <- c("div_type", "div_name")
+  # div_df <- matrix(c("obs_richness", "Observed Species Richness",
+  #                    "cum_richness", "Cumulative Species Richness",
+  #                    "total_occ", "Total Occurrences",
+  #                    "occ_by_type", "Total Occurrences Segrerated by Type",
+  #                    "occ_by_dataset", "Total Occurrences Segregated by Dataset",
+  #                    "rarefied", "Estimated Species Richness",
+  #                    "hill0", "Estimated Species Richness",
+  #                    "hill1", "Estimated Shannon-Hill Diversity",
+  #                    "hill2", "Estimated Simpson-Hill Diversity",
+  #                    "e9_evenness", "E9 Evenness",
+  #                    "pielou_evenness", "Pielou's Evenness",
+  #                    "species_rarity", "Species Rarity",
+  #                    "newness", "Mean Year of Occurrences",
+  #                    "density", "Density of Occurrences",
+  #                    "ab_rarity", "Abundance-Based Rarity",
+  #                    "area_rarity", "Area-Based Rarity",
+  #                    "spec_occ", "Species Occurrences",
+  #                    "spec_range", "Species Ranges",
+  #                    "tax_distinct", "Taxonomic Distinctness"
+  #                 ),
+  #                 ncol = 2,
+  #                 byrow = TRUE) %>%
+  #   as.data.frame
 
-  div_name <- div_df$div_name[div_df$div_type %in% x]
+  # names(div_df) <- c("div_type", "div_name")
+  #
+  # div_name <- div_df$div_name[div_df$div_type %in% x]
+  #
+  # if(!length(div_name) > 0) stop("div_type not found")
 
-  if(!length(div_name) > 0) stop("div_type not found")
-
-  return(div_name)
+  return(iname)
 
 }
