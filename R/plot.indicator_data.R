639631d9241bd61b7e32a1bdf6b70b7da4011cab
Shawn Dove <shawn.dove@hotmail.com>
2024-03-23 13:32:23 +0100

    function file restructuring

--- R/plot.indicator_data.R
+++
@@ -1,6 +0,0 @@
-plot.indicator_data <- function(x, ...) {
-  warning("Plotting directly the data frame of an indicator_map or indicator_ts
-          object ignores metadata and may not give the intended output.
-          Consider plotting the indicator_map or indicator_ts object instead.")
-  NextMethod()
-}
