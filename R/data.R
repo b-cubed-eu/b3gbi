944281a80a120ddce23eba000c59dfff12b93b87
Shawn Dove <shawn.dove@hotmail.com>
2024-04-13 00:18:18 +0200

    all data objects now documented

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Cube of GBIF Mammal Occurrences in Denmark
 #'
-#' Small example cube containing the mammal occurrences in Denmark favailable on GBIF as of 16.03.2024.
+#' Small example cube containing the mammal occurrences in Denmark available on GBIF as of 16.03.2024.
 #'
 #' @format A 'processed_cube' object containing a tibble with 276,950 rows and 10 variables, as well as metadata
 #' \describe{
@@ -14,6 +14,13 @@
 #'   \item{xcoord}{East-West coordinate on the EEA grid}
 #'   \item{ycoord}{North-South coordinate on the EEA grid}
 #'   \item{resolution}{grid cell size}
+#' }
+#' @examples{
+#'   \dontrun{
+#'     # Basic example of how to use the dataset
+#'     denmark_mammals_or_map <- obs_richness_map(example_cube_1, level = "country", region = "Denmark")
+#'     plot(denmark_mammals_or_map, title = "Mammals in Denmark (1751-2023): Observed Species Richness")
+#'   }
 #' }
 #' @source \url{https://doi.org/10.15468/dl.5mb887}
 "example_cube_1"
@@ -35,51 +42,84 @@
 #'   \item{ycoord}{North-South coordinate on the EEA grid}
 #'   \item{resolution}{grid cell size}
 #' }
-#' @source \url{https://doi.org/10.15468/dl.qk4f2z}
-"example_cube_2"
-
-
-#'
-#' \name{example_cube_1}
-#' \alias{example_cube_1}
-#' \title{Cube of GBIF Mammal Occurrences in Denmark}
-#' \description{
-#'   This is a small example cube containing the mammal occurrences in Denmark available on GBIF as of 16.03.2024.
-#' }
-#' \format{
-#'   Contains a tibble with the columns 'year', 'eea_cell_code', 'taxonKey', 'obs', 'scientificName', 'rank', 'kingdom', 'xcoord', 'ycoord', and 'resolution'. This can be directly accessed using example_cube_1$data. Also contains metadata, including the data range, resolution, number of grid cells, coordinate range, total number of observations, number of species represented, and kingdoms represented. Simply type example_cube_1 to see the metadata.
-#' }
-#' \source{
-#'   The occurrences were downloaded from GBIF: https://doi.org/10.15468/dl.5mb887
-#' }
-#' \examples{
-#'   \dontrun{
-#'     # Basic example of how to use the dataset
-#'     denmark_mammals_or_map <- obs_richness_map(example_cube_1, level = "country", region = "Denmark")
-#'     plot(denmark_mammals_or_map, title = "Mammals in Denmark (1751-2023): Observed Species Richness")
-#'   }
-#' }
-#'
-#'
-#'
-#' \name{example_cube_2}
-#' \alias{example_cube_2}
-#' \title{Cube of GBIF Insect Occurrences in Europe}
-#' \description{
-#'   This is a small example cube containing the insect occurrences in Europe available on GBIF as of 16.03.2024.
-#' }
-#' \format{
-#'   Contains a tibble with the columns 'year', 'eea_cell_code', 'taxonKey', 'obs', 'scientificName', 'rank', 'kingdom', 'xcoord', 'ycoord', and 'resolution'. This can be directly accessed using example_cube_1$data. Also contains metadata, including the data range, resolution, number of grid cells, coordinate range, total number of observations, number of species represented, and kingdoms represented. Simply type example_cube_2 to see the metadata.
-#' }
-#' \source{
-#'   The occurrences were downloaded from GBIF: https://doi.org/10.15468/dl.qk4f2z
-#'   \examples{
+#' @examples{
 #'     \dontrun{
 #'       # Basic example of how to use the dataset
 #'       europe_insects_or_map <- obs_richness_map(example_cube_2, level = "continent", region = "Europe")
 #'       plot(europe_insects_or_map, title = "Insects in Europe (1751-2023): Observed Species Richness")
 #'     }
 #'   }
+#' @source \url{https://doi.org/10.15468/dl.qk4f2z}
+"example_cube_2"
+
+
+#' Map of Pielou's Evenness for Mammals in Denmark
 #'
+#' Example indicator containing a map of pielou's evenness for mammal occurrences in Denmark (occurrences from GBIF: 16.03.2024).
 #'
+#' @format An 'indicator_map' object containing a tibble with 576 rows and 4 variables, as well as metadata
+#' \describe{
+#'   \item{cellid}{ID number identifying a particular grid cell}
+#'   \item{area_km2}{area of the grid cell in square kms}
+#'   \item{diversity_val}{calculated evenness value for the grid cell}
+#'   \item{geometry}{grid cell polygon (this column is not visible by default when you print the object)}
+#' }
+#' @source \url{https://doi.org/10.15468/dl.qk4f2z}
+"example_indicator_map1"
+
+#' Map of Abundance-Based Rarity for Insects in Europe
 #'
+#' Example indicator containing a map of abundance-based rarity for insect occurrences in Europe (occurrences from GBIF: 16.03.2024).
+#'
+#' @format An 'indicator_map' object containing a tibble with 3,225 rows and 4 variables, as well as metadata
+#' \describe{
+#'   \item{cellid}{ID number identifying a particular grid cell}
+#'   \item{area_km2}{area of the grid cell in square kms}
+#'   \item{diversity_val}{calculated rarity value for the grid cell}
+#'   \item{geometry}{grid cell polygon (this column is not visible by default when you print the object)}
+#' }
+#' @source \url{https://doi.org/10.15468/dl.qk4f2z}
+"example_indicator_map2"
+
+#' Time Series of Observed Species Richness for Mammals in Denmark
+#'
+#' Example indicator containing a time series of observed species richness for mammal occurrences in Denmark (occurrences from GBIF: 16.03.2024).
+#'
+#' @format An 'indicator_ts' object containing a tibble with 54 rows and 2 variables, as well as metadata
+#' \describe{
+#'   \item{year}{a year the indicator was calculated for}
+#'   \item{diversity_val}{calculated richness value for the year}
+#' }
+#' @source \url{https://doi.org/10.15468/dl.qk4f2z}
+"example_indicator_ts1"
+
+#' Time Series of Cumulative Species Richness for Insects in Europe
+#'
+#' Example indicator containing a time series of cumulative species richness for insect occurrences in Europe (occurrences from GBIF: 16.03.2024).
+#'
+#' @format An 'indicator_ts' object containing a tibble with 89 rows and 2 variables, as well as metadata
+#' \describe{
+#'   \item{year}{a year the indicator was calculated for}
+#'   \item{diversity_val}{calculated richness value for the year}
+#' }
+#' @source \url{https://doi.org/10.15468/dl.qk4f2z}
+"example_indicator_ts2"
+
+#' Indicators Available for Use in the Package
+#'
+#' A list of all biodiversity indicators available within the package, along with the dimensions they can be calculated across, the functions to access them, and any special arguments
+#'
+#' @format A special object of class 'available_indicators' containing a list of indicators and six fields with information about them
+#' \describe{
+#'   \item{indicator_class}{class of the indicator}
+#'   \item{indicator_name}{name of the indicator}
+#'   \item{plot_title}{title to be used when plotting with automated title generation}
+#'   \item{legend_label}{title to be used when plotting with automated legend title generation}
+#'   \item{legend_transformation}{any transformation to perform on the legend when plotting, to improve visualization of maps}
+#'   \item{map_wrapper}{wrapper function to use when calculating indicator as a map}
+#'   \item{ts_wrapper}{wrapper function to use when calculating indicator as a time series}
+#'   \item{map_function_arguments}{any special arguments to consider when using the function to calculate an indicator map}
+#'   \item{ts_function_arguments}{any special arguments to consider when using the function to calculate an indicator time series}
+#' }
+#' @source N/A
+"available_indicators"
