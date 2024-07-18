17b8ab01a6ab7cd81717b3b4601fc17c49f27fa7
Shawn Dove <shawn.dove@hotmail.com>
2024-07-18 12:05:08 +0200

    added an extra class 'sim_cube'for objects without grid codes

--- R/constructor_functions.R
+++ R/constructor_functions.R
@@ -66,6 +66,35 @@ new_processed_cube <- function(x, grid_type) {
   }
 }
 
+#' @title 'sim_cube' S3 Constructor
+#'
+#' @description This function constructs a 'sim_cube' S3 object, a specialized data
+#' structure designed for analysis of simulated biodiversity data (e.g. from the gcube
+#' package) that lacks spatial information. It validates the input data cube, calculates
+#' essential summary information, and prepares the object for further use.
+#'
+#' @param x A tibble data cube containing simulated occurrence data.
+#'
+#' @return  A 'gcube' S3 object containing:
+#'   * **Summary statistics:** First and last year of data, number of species, number of observations.
+#'   * **Original data:** The input data cube.
+#'
+#' @noRd
+new_sim_cube <- function(x, grid_type) {
+  # check that x is a tibble and all necessary columns are present
+  stopifnot(tibble::is_tibble(x),
+            all(c("year",
+                  "taxonKey",
+                  "obs") %in% names(x)))
+    structure(list(first_year = min(x$year),
+                   last_year = max(x$year),
+                   num_species = length(unique(x$taxonKey)),
+                   num_obs = sum(x$obs),
+                   grid_type = grid_type,
+                   data = x),
+              class = "sim_cube")
+}
+
 
 #' @title 'indicator_ts' S3 Constructor
 #'
