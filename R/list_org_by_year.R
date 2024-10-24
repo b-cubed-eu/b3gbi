036e3666a2f2a1a1c3f3a8a0da2cf5fed6abea8a
Shawn Dove <shawn.dove@hotmail.com>
2024-10-24 17:29:56 +0200

    create list of vectors organized by year

---
+++ R/list_org_by_year.R
@@ -0,0 +1,21 @@
+# Put individual observations into a list organized by year
+# col should be a column name in quotes, e.g. "obs"
+#' @noRd
+list_org_by_year <- function(df, col) {
+
+  # create empty list
+  a <- list()
+  # set counter j to 1
+  j <- 1
+  # iterate over unique years
+  for (i in unique(df$year)) {
+    # add observations from year i to list
+    a[[j]] <- df[[col]][df$year==i]
+    # iterate counter
+    j <- j + 1
+  }
+  # name list elements
+  names(a) <- unique(df$year)
+
+  return(a)
+}
