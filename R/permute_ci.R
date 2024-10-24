5a6b4ad90ad6cd3c6f2fb979250ed635a904d194
Shawn Dove <shawn.dove@hotmail.com>
2024-10-24 17:30:22 +0200

    create CIs via permutations

---
+++ R/permute_ci.R
@@ -0,0 +1,15 @@
+# Calculate confidence intervals by permutation
+#' @noRd
+permute_ci <- function(x, num_bootstrap) {
+
+  n <- nrow(x)
+  n_seq <- seq_len(n)
+  val_sample <- replicate(num_bootstrap,
+                              resample(x$unique_by_year, n, replace = TRUE))
+  index <- t(apply(val_sample, 2, cumsum))
+  # index_df <- do.call(rbind, index)
+  lower.ci <- apply(index, 2, quantile, 0.05)
+  upper.ci <- apply(index, 2, quantile, 0.95)
+  df <- data.frame(year = x$year, ll = lower.ci, ul = upper.ci)
+
+}
