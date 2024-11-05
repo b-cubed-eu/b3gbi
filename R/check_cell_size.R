e4340b88a91ec24bd6cd17dd35c53b8fdae5d9c7
Shawn Dove <43437482+shawndove@users.noreply.github.com>
2024-11-05 11:38:28 +0100

    fix error when user-provided cell size is the same as existing resolution

--- R/check_cell_size.R
+++ R/check_cell_size.R
@@ -13,8 +13,7 @@ check_cell_size <- function(cell_size, cell_size_units, resolution, level) {
 
         res_size <- as.numeric(stringr::str_extract(resolution, "[0-9]*(?=km)"))
 
-        if ((res_size < cell_size) |
-            as.integer(cell_size / res_size)==(cell_size / res_size)) {
+        if (res_size %% cell_size != 0) {
 
           stop("cell_size must be a whole number multiple of the resolution.
                For example, if resolution is 1 km, cell_size can be 1, 2, 3,.. 10,.. 100, etc.")
