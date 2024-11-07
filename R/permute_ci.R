4df3b266737dd23420a39825870507b9abfdb312
Shawn Dove <shawn.dove@hotmail.com>
2024-11-07 13:19:22 +0100

    make cumsum optional

--- R/permute_ci.R
+++ R/permute_ci.R
@@ -1,12 +1,22 @@
 # Calculate confidence intervals by permutation
 #' @noRd
-permute_ci <- function(x, num_bootstrap) {
+permute_ci <- function(x, num_bootstrap, cumsum = FALSE) {
 
   n <- nrow(x)
   n_seq <- seq_len(n)
   val_sample <- replicate(num_bootstrap,
                               resample(x$unique_by_year, n, replace = TRUE))
-  index <- t(apply(val_sample, 2, cumsum))
+
+  if (cumsum==TRUE) {
+
+    index <- t(apply(val_sample, 2, cumsum))
+
+  } else {
+
+    index <- t(val_sample)
+
+  }
+
   # index_df <- do.call(rbind, index)
   lower.ci <- apply(index, 2, quantile, 0.05)
   upper.ci <- apply(index, 2, quantile, 0.95)
