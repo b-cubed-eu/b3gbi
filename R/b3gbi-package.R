00f9db4dcb03b36a4681a38c9e68737cb352820c
Shawn Dove <shawn.dove@hotmail.com>
2025-02-19 15:57:24 +0100

    remove redundant importFrom statements

--- R/b3gbi-package.R
+++ R/b3gbi-package.R
@@ -3,11 +3,7 @@
 
 ## usethis namespace: start
 #' @import ggplot2
-#' @importFrom purrr map keep map2
-#' @importFrom readr read_csv
-#' @importFrom dplyr group_split mutate bind_rows distinct select n_distinct
-#' @importFrom dplyr group_by summarise select arrange full_join inner_join left_join
-#' @importFrom dplyr rename filter slice_max across any_of
+#' @importFrom dplyr %>% .data
 #' @importFrom tidyr pivot_wider pivot_longer gather spread
 #' @importFrom ggplot2 ggplot geom_sf geom_smooth geom_ribbon geom_errorbar geom_point geom_line
 #' @importFrom tibble rownames_to_column column_to_rownames
