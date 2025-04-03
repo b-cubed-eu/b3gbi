3be42855e5e3a43f7b56188b9f751a993e96140c
Shawn Dove <shawn.dove@hotmail.com>
2025-04-03 09:07:18 +0200

    add out of bounds indices handling and NA handling

--- R/boot_statistic_sum.R
+++ R/boot_statistic_sum.R
@@ -1,6 +1,9 @@
 # boot statistic function to sum the data
 #' @noRd
 boot_statistic_sum <- function(data, indices) {
+  if (any(indices > length(data)) || any(indices < 1)) {
+    stop("Indices contain out-of-bounds values.")
+  }
   d <- data[indices]
-  return(as.numeric(sum(d)))
+  return(as.numeric(sum(d, na.rm = TRUE)))
 }
