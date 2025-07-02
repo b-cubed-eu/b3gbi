6d64027f168e601a5a761f19caaea6ee4be324d5
Shawn Dove <43437482+shawndove@users.noreply.github.com>
2025-07-02 11:40:44 +0200

    fix issues with floating point for resolutions less than 1

--- R/check_cell_size.R
+++ R/check_cell_size.R
@@ -16,16 +16,16 @@ check_cell_size <- function(cell_size, resolution, level) {
 
   if (!is.null(cell_size)) {
     if (stringr::str_detect(resolution, "km")) {
-      res_size <- as.numeric(stringr::str_extract(resolution, "[0-9]*(?=km)"))
-      if (cell_size %% res_size != 0) {
+      res_size <- as.numeric(stringr::str_extract(resolution, "[0-9.]+(?=km)"))
+      if (!isTRUE(all.equal(cell_size/res_size, round(cell_size/res_size)))) {
         stop("cell_size must be a whole number multiple of the resolution.
              For example, if resolution is 1 km, cell_size can be 1, 2, 3,.. 10,.. 100, etc.")
       }
       # convert to meters
       cell_size <- cell_size * 1000
     } else if (stringr::str_detect(resolution, "degrees")) {
-      res_size <- as.numeric(stringr::str_extract(resolution, "[0-9,.]*(?=degrees)"))
-      if (cell_size %% res_size != 0) {
+      res_size <- as.numeric(stringr::str_extract(resolution, "[0-9.]+(?=degrees)"))
+      if (!isTRUE(all.equal(cell_size/res_size, round(cell_size/res_size)))) {
         stop("cell_size must be a whole number multiple of the resolution.
              For example, if resolution is 0.25 degrees, cell_size can be 0.25, 0.5, 0.75, 1, etc.")
       }
@@ -39,7 +39,7 @@ check_cell_size <- function(cell_size, resolution, level) {
       # convert to meters
       cell_size <- cell_size * 1000
     } else if (stringr::str_detect(resolution, "degrees")) {
-      res_size <- as.numeric(stringr::str_extract(resolution, "[0-9,.]*(?=degrees)"))
+      res_size <- as.numeric(stringr::str_extract(resolution, "[0-9.]+(?=degrees)"))
       if (res_size < 1) {
         cont_res_size <- 1
       } else {
