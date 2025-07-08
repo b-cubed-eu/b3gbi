0b328f9d57a5396795a3af02746664700beddb6c
Shawn Dove <43437482+shawndove@users.noreply.github.com>
2025-07-08 09:38:21 +0200

    minor update, nothing important

--- run_ramsar_metrics.R
+++ run_ramsar_metrics.R
@@ -1,8 +1,9 @@
 inputdir <- "inst/extdata/ramsar_site_data_100m_asia"
 maindir <- "output/ramsar_metric_results_100m/asia"
 shapefiledir <- "inst/extdata/ramsar_sites_wkt"
+continent <- substring(inputdir, 36, nchar(inputdir))
 
-countrylist <- list.files(inputdir)[1]
+countrylist <- list.files(inputdir)[2]
 for (i in 1:length(countrylist)) {
 
   countrydir <- paste0(maindir, "/", countrylist[i])
@@ -23,17 +24,17 @@ for (i in 1:length(countrylist)) {
   for (j in 1:length(sitelist)) {
 
     sitename <- substring(sitelist[j], 1, nchar(sitelist[j]) - 9)
-    sitedir <- paste0(countrydir, "/", sitename)
-    if (!dir.exists(sitedir)) {
-      dir.create(sitedir, recursive = TRUE)
-      message(paste("Created ", sitelist[j], " output directory:", sitedir))
-    }
+    # sitedir <- paste0(countrydir, "/", sitename)
+    # if (!dir.exists(sitedir)) {
+    #   dir.create(sitedir, recursive = TRUE)
+    #   message(paste("Created ", sitelist[j], " output directory:", sitedir))
+    # }
 
     shapefilepath <- paste0(shapefiledir, "/", countrylist[i], "/", sitename, ".wkt")
 
     temp <- process_cube(paste0(inputdir, "/", countrylist[i], "/", sitelist[j]), separator = ",")
-    temp_obsrich_map <- obs_richness_map(temp, cell_size = 0.1, shapefile_path = shapefilepath)
-    temp_orm_plot <- plot(temp_obsrich_map)
+    temp_obsrich_map <- obs_richness_map(temp, cell_size = 0.1, shapefile_path = shapefilepath, ne_scale = "medium", region = continent)
+    temp_orm_plot <- plot(temp_obsrich_map, crop_to_grid = TRUE)
 
     ggsave(filename = paste0(ormapsdir, "/", sitename, ".png"), temp_orm_plot, device = "png", height = 8000, width = 8000, units = "px")
 
