3ffcf61b11141fbd03aac7fbc5a95ec77c7c3849
Shawn Dove <43437482+shawndove@users.noreply.github.com>
2025-08-01 11:19:30 +0200

    incorporate layers to take advantage of new code update

--- run_ramsar_metrics.R
+++ run_ramsar_metrics.R
@@ -3,7 +3,7 @@ maindir <- "output/ramsar_metric_results_100m/asia"
 shapefiledir <- "inst/extdata/ramsar_sites_wkt"
 continent <- substring(inputdir, 36, nchar(inputdir))
 
-countrylist <- list.files(inputdir)[6]
+countrylist <- list.files(inputdir)[10]
 for (i in 1:length(countrylist)) {
 
   countrydir <- paste0(maindir, "/", countrylist[i])
@@ -18,9 +18,9 @@ for (i in 1:length(countrylist)) {
   ormapsdir <- paste0(maindir, "/plots/obs_rich_maps/", countrylist[i])
   if (!dir.exists(ormapsdir)) {
     dir.create(ormapsdir, recursive = TRUE)
-    message(paste("Created ", sitelist[j], " observed richness maps directory:", ormapsdir))
+    message(paste("Created ", sitelist[i], " observed richness maps directory:", ormapsdir))
   }
-for (j in 3:3) {
+ for (j in 41:length(sitelist)) {
 #  for (j in 1:length(sitelist)) {
 
     tryCatch({
@@ -34,11 +34,17 @@ for (j in 3:3) {
 
       shapefilepath <- paste0(shapefiledir, "/", countrylist[i], "/", sitename, ".wkt")
 
-      temp <- process_cube(paste0(inputdir, "/", countrylist[i], "/", sitelist[j]), separator = ",")
-      temp_obsrich_map <- obs_richness_map(temp, cell_size = 1, shapefile_path = shapefilepath, ne_scale = "large", region = continent)
-      temp_orm_plot <- plot(temp_obsrich_map, crop_to_grid = TRUE)
+      temp <- process_cube(paste0(inputdir, "/", countrylist[i], "/", sitelist[j]),
+                           separator = ",")
+      temp_obsrich_map <- obs_richness_map(temp,
+                                           shapefile_path = shapefilepath,
+                                           ne_scale = "large",
+                                           region = continent,
+                                           layers = c("ocean", "lakes", "rivers_lake_centerlines")
+                                           )
+      temp_orm_plot <- plot(temp_obsrich_map)
 
-      ggsave(filename = paste0(ormapsdir, "/", sitename, ".png"), temp_orm_plot, device = "png", height = 12000, width = 12000, units = "px")
+      ggsave(filename = paste0(ormapsdir, "/", sitename, ".png"), temp_orm_plot, device = "png", height = 4000, width = 4000, units = "px")
 
     }, error = function(e) {
       if (grepl("No spatial intersection between map data and grid.", e) ||
