b75b6b851696b02aa34557c68e610d62835b7dc0
Shawn Dove <43437482+shawndove@users.noreply.github.com>
2025-08-13 18:36:50 +0200

    name changes

--- R/add_NE_layer.R
+++ R/add_NE_layer.R
@@ -1,11 +1,11 @@
-add_NE_layer <- function(layer_name, scale, latlong_extent) {
+add_NE_layer <- function(layer_name, scale, extent_projected) {
   # Load necessary packages if not already loaded (good practice for functions)
   if (!requireNamespace("rnaturalearth", quietly = TRUE)) stop("rnaturalearth package required.")
   if (!requireNamespace("sf", quietly = TRUE)) stop("sf package required.")
   if (!requireNamespace("dplyr", quietly = TRUE)) stop("dplyr package required.")
   if (!requireNamespace("rlang", quietly = TRUE)) stop("rlang package required for dynamic filtering.") # Added rlang
 
-  geometry <- featurecla <- scalerank <- min_zoom <- type <- NULL
+  geometry <- featurecla <- scalerank <- type <- NULL
 
   if (layer_name == "admin_0_countries") {
     # Use the robust ne_countries function for the base layer
@@ -70,24 +70,24 @@ add_NE_layer <- function(layer_name, scale, latlong_extent) {
 
   }
 
-  le_projected <- sf::st_transform(sf::st_as_sfc(latlong_extent),
-                                   crs = "+proj=eck4 +datum=WGS84")
+  # le_projected <- sf::st_transform(sf::st_as_sfc(latlong_extent),
+  #                                  crs = "ESRI:54012")
 
   # Attempt to perform cropping for efficiency FIRST
   # If it fails, validate first
   processed_layer <- tryCatch({
     layer_raw %>%
-      sf::st_transform(crs = "+proj=eck4 +datum=WGS84") %>%
-      group_by(scalerank, featurecla, min_zoom) %>%
-      dplyr::summarize(geometry = sf::st_crop(geometry, le_projected)) %>%
-      sf::st_transform(crs = sf::st_crs(latlong_extent))
+      sf::st_transform(crs = "ESRI:54012") %>%
+      group_by(scalerank, featurecla) %>%
+      dplyr::summarize(geometry = sf::st_crop(geometry, extent_projected))# %>%
+    #  sf::st_transform(crs = sf::st_crs(latlong_extent))
   }, error = function(e) {
     layer_raw %>%
       sf::st_make_valid() %>%
-      sf::st_transform(crs = "+proj=eck4 +datum=WGS84") %>%
-      group_by(scalerank, featurecla, min_zoom) %>%
-      dplyr::summarize(geometry = sf::st_crop(geometry, le_projected)) %>%
-      sf::st_transform(crs = sf::st_crs(latlong_extent))
+      sf::st_transform(crs = "ESRI:54012") %>%
+      group_by(scalerank, featurecla) %>%
+      dplyr::summarize(geometry = sf::st_crop(geometry, extent_projected))# %>%
+     # sf::st_transform(crs = sf::st_crs(latlong_extent))
   })
 
   # Then validate and filter (on the now smaller dataset)
