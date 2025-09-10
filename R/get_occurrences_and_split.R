4e4e062f6616b54a1ee18d46294ba28070d5bdb7
Shawn Dove <shawn.dove@hotmail.com>
2025-09-10 12:20:48 +0200

    refactor repeated code into functions and add needed NULL assignments

--- R/get_occurrences_and_split.R
+++ R/get_occurrences_and_split.R
@@ -2,48 +2,52 @@
 #' @noRd
 get_occurrences_and_split <- function(data, filter_type, species) {
 
+  taxonKey <- scientificName <- NULL
+
   errormsg <- paste0(
     "No matching ", filter_type, "s. Please check that you have entered ",
     "them correctly."
   )
 
- if (filter_type == "taxonKey") {
+  check_for_empty <- function(data, errormsg) {
+    if (nrow(data) < 1) stop(errormsg)
+    return(data)
+  }
 
-   # Get occurrences for selected species
-   species_occurrences <- data %>%
-     dplyr::filter(taxonKey %in% species) %>%
-     {
-       if (nrow(.) < 1) stop(errormsg) else (.)
-     } %>%
-     dplyr::mutate(taxonKey = factor(taxonKey, levels = unique(taxonKey)))
+  starts_with <- function(string, patterns) {
+    regex <- paste("^", patterns, collapse = "|", sep = "")
+    grepl(regex, string)
+  }
 
-   split_so <-
-     species_occurrences %>%
-     dplyr::group_split(taxonKey)
+  split_by_column <- function(data, col)
+  {
+    data %>%
+      dplyr::group_split({{ col }})
+  }
 
- } else if (filter_type == "scientificName") {
+  if (filter_type == "taxonKey") {
 
-   # Get occurrences for selected species
-   species_occurrences <- data %>%
-     dplyr::filter(
-       grepl(paste("^", species, collapse = "|", sep = ""), scientificName)) %>%
-     {
-       if (nrow(.) < 1) stop(errormsg) else (.)
-     } %>%
-     dplyr::arrange(
-       scientificName,
-       grepl(paste("^", species, collapse = "|", sep = ""), scientificName)
-     )
+    # Get occurrences for selected species and split by taxonKey
+    split_so <- data %>%
+      dplyr::filter(taxonKey %in% species) %>%
+      check_for_empty(errormsg) %>%
+      dplyr::mutate(taxonKey = factor(taxonKey, levels = unique(taxonKey))) %>%
+      split_by_column(taxonKey)
 
-   split_so <-
-     species_occurrences %>%
-     dplyr::group_split(scientificName)
+  } else if (filter_type == "scientificName") {
 
- } else {
+    # Get occurrences for selected species and split by scientificName
+    split_so <- data %>%
+      dplyr::filter(starts_with(scientificName, species)) %>%
+      check_for_empty(errormsg) %>%
+      dplyr::arrange(scientificName, starts_with(scientificName, species)) %>%
+      split_by_column(scientificName)
 
-   stop("filter_type must be either taxonKey or species.")
+  } else {
 
- }
+    stop("filter_type must be either taxonKey or species.")
+
+  }
 
   return(split_so)
 
