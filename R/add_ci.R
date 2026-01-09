1f6584cbaf6ab519214a5cae5d324efa22bd7a2c
Shawn Dove <shawn.dove@hotmail.com>
2026-01-09 13:48:37 +0100

    fix documentation, add missing null assignments, and fix incorrectly named object

--- R/add_ci.R
+++ R/add_ci.R
@@ -8,20 +8,19 @@
 #' CI calculation process.
 #'
 #' @param indicator An object of class `indicator_ts` to which confidence
-#' intervals should be added.
+#'   intervals should be added.
 #' @param num_bootstrap (Optional) Number of bootstrap replicates to perform.
-#' (Default: 1000)
+#'   (Default: 1000)
 #' @param bootstrap_level (Optional) Level at which to perform bootstrapping:
 #' \itemize{
 #'   \item \code{'cube'} (default): Bootstrapping is done by resampling the
-#'   occurrence records in the cube. This is mathematically more robust as it
-#'   captures the underlying sampling uncertainty.
+#'     occurrence records in the cube. This is mathematically more robust as it
+#'     captures the underlying sampling uncertainty.
 #'   \item \code{'indicator'}: Bootstrapping is done by resampling indicator
-#'    values. This is faster for large cubes but less robust..
+#'     values. This is faster for large cubes but less robust.
 #' }
-
 #' @param ci_type (Optional) Type of bootstrap confidence intervals to
-#' calculate. (Default: \code{"norm"}). Supported options are:
+#'   calculate. (Default: \code{"norm"}). Supported options are:
 #' \itemize{
 #'   \item \code{'norm'}: Normal approximation intervals.
 #'   \item \code{'basic'}: Basic bootstrap intervals.
@@ -30,16 +29,16 @@
 #'   \item \code{'none'}: No confidence intervals calculated.
 #' }
 #' @param trans (Optional) A function for transforming the indicator values
-#' before calculating confidence intervals (e.g., \code{log}).
-#' (Default: identity function)
+#'   before calculating confidence intervals (e.g., \code{log}).
+#'   (Default: identity function)
 #' @param inv_trans (Optional) The inverse of the transformation function
-#' \code{trans} (e.g., \code{exp}). Used to back-transform the intervals
-#' to the original scale. (Default: identity function)
+#'   \code{trans} (e.g., \code{exp}). Used to back-transform the intervals
+#'   to the original scale. (Default: identity function)
 #' @param confidence_level (Optional) The confidence level for the calculated
-#' intervals (e.g., 0.95 for 95\% CIs). (Default: 0.95)
+#'   intervals (e.g., 0.95 for 95\% CIs). (Default: 0.95)
 #' @param overwrite (Optional) Logical. If the indicator already contains
-#' confidence intervals (\code{ll} and \code{ul} columns), should they
-#' be replaced? (Default: TRUE)
+#'   confidence intervals (\code{ll} and \code{ul} columns), should they
+#'   be replaced? (Default: TRUE)
 #'
 #' @details
 #' The function acts as a bridge to the \code{dubicube} package for statistical
@@ -49,7 +48,7 @@
 #' a warning is issued and the original object is returned.
 #'
 #' @return An updated object of class \code{indicator_ts} containing the
-#' original data with the following additional columns:
+#'   original data with the following additional columns:
 #' \itemize{
 #'   \item \code{ll}: Lower limit of the confidence interval.
 #'   \item \code{ul}: Upper limit of the confidence interval.
@@ -62,7 +61,6 @@
 #'
 #' @seealso \code{\link[dubicube]{bootstrap_cube}}, \code{\link[dubicube]{calculate_bootstrap_ci}}
 #'
-#' @rdname add_ci
 #' @export
 add_ci <- function(indicator,
                    num_bootstrap = 1000,
@@ -76,12 +74,15 @@ add_ci <- function(indicator,
                    trans = function(t) t,
                    inv_trans = function(t) t,
                    confidence_level = 0.95,
-                   overwrite = TRUE) {
+                   overwrite = TRUE,
+                   ...) {
 
   # Check for correct object class
   if (!inherits(indicator, "indicator_ts")) {
     stop("indicator must be an indicator_ts object.")
   }
+
+  ll <- ul <- year <- est_original <- NULL
 
   # List of indicators for which bootstrapped confidence intervals should not
   # be calculated
@@ -143,7 +144,7 @@ add_ci <- function(indicator,
   if (bootstrap_level == "indicator") {
 
     # Send data to calc_ci for indicator level bootstrapping
-    indicator <- calc_ci(data_final_nogeom,
+    indicator <- calc_ci(raw_data,
                          indicator = indicator,
                          num_bootstrap = num_bootstrap,
                          ci_type = ci_type,
