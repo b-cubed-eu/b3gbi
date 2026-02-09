4011d19286e4d815b29fffd974a9133e672183f3
Shawn Dove <43437482+shawndove@users.noreply.github.com>
2026-02-09 14:32:20 +0100

    fix accidental rename of bootstrap_results

--- R/add_ci.R
+++ R/add_ci.R
@@ -218,7 +218,7 @@ add_ci <- function(indicator,
                                  params_total$bootstrap_params)
 
     # Calculate confidence intervals from bootstrap results
-    params_total$ci_params$bootstrap_samples_df <- bootstrap_results
+    params_total$ci_params$bootstrap_results <- bootstrap_results
     group_cols <- params_total$ci_params$grouping_var
     ci_df <- do.call(
       dubicube::calculate_bootstrap_ci,
